b6a3d48343e85fdec02fd1f6157c3fda907f87d3
zicosig <kouadioazicuvci@gmail.com>
2026-07-21 12:04:19 +0000

    Update supabase.js

--- www/supabase.js
+++ www/supabase.js
@@ -1,6 +1,9 @@
+// Connexion Supabase Registre Planteurs
+
 const SUPABASE_URL = "https://qmfnnykocjmwemsbinhf.supabase.co";
 
-const SUPABASE_KEY = "TA_CLE_ANON_ICI";
+const SUPABASE_KEY = "sb_publishable_GfjFAsmjicRFYwIBRfsNwg_Cs2xKm1v";
+
 
 window.supabaseClient = supabase.createClient(
     SUPABASE_URL,
