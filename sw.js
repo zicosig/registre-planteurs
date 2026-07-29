5087f4041f34c033b27b0114b6984b2bd74236a8
zicosig <kouadioazicuvci@gmail.com>
2026-07-29 22:24:56 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Incrémentez VERSION à chaque mise en ligne d'une nouvelle version
    du fichier index.html : c'est ce qui déclenche la mise à jour
    sur les téléphones des agents. */
-const VERSION = "rp-2026-07-29-1";
+const VERSION = "rp-2026-07-29-cloud-v8";
 const SOCLE = [
   "./",
   "./index.html",
